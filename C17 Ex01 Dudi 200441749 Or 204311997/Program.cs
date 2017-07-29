f95627bcf97d646b38c2327998ba439278c21369
Dudiy <dudi.2705@gmail.com>
2017-07-29 11:11:35 +0300

    Add project files.

---
+++ C17 Ex01 Dudi 200441749 Or 204311997/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace C17_Ex01_Dudi_200441749_Or_204311997
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
