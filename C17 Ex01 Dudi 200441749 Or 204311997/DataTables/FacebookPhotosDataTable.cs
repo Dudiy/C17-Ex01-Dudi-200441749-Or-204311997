fd53de2b4707a4f3b7779ef1e7e29c733349692e
Dudiy <dudi.2705@gmail.com>
2017-08-05 13:45:06 +0300

    updated facebookPhotosDataTable

--- C17 Ex01 Dudi 200441749 Or 204311997/DataTables/FacebookPhotosDataTable.cs
+++ C17 Ex01 Dudi 200441749 Or 204311997/DataTables/FacebookPhotosDataTable.cs
@@ -18,42 +18,46 @@ namespace C17_Ex01_Dudi_200441749_Or_204311997.DataTables
         {
             DataTable.Clear();
             TotalRows = 0;
-            foreach (Album album in AlbumsToLoad)
-            {
-                TotalRows += album.Count != null ? (int)album.Count : 0;
-            }
 
             //add rows
-            ProgressBarWindow progressBarWindow = new ProgressBarWindow(0, TotalRows);
-            progressBarWindow.Show();
-            foreach (Album album in AlbumsToLoad)
+            if (AlbumsToLoad.Count > 0)
             {
-                foreach (Photo photo in album.Photos)
+                foreach (Album album in AlbumsToLoad)
                 {
-                    StringBuilder photoTags = new StringBuilder();
-                    if (photo.Tags != null)
-                    {
-                        foreach (PhotoTag tag in photo.Tags)
-                        {
-                            photoTags.Append(tag.User.Name);
-                            photoTags.Append(", ");
-                        }
-                        photoTags.Remove(photoTags.Length - 2, 2);
-                    }
+                    TotalRows += album.Count != null ? (int)album.Count : 0;
+                }
 
-                    DataTable.Rows.Add(
-                        photo,
-                        photo.Album.Name,
-                        //TODO get most liked comment
-                        photo.CreatedTime != null ? photo.CreatedTime : null,   //TODO fix format
-                        photo.LikedBy != null ? photo.LikedBy.Count : 0,
-                        photo.Comments != null ? photo.Comments.Count : 0,
-                        photo.Tags != null ? photoTags.ToString() : string.Empty,
-                        photo.URL
-                        );
-                    progressBarWindow.ProgressValue++;
-                    //PhotosDataLoadStatus = (float)++counter / totalRows;
-                    //PhotosDataLoadStatus.Value++;
+                ProgressBarWindow progressBarWindow = new ProgressBarWindow(0, TotalRows);
+                progressBarWindow.Show();
+                foreach (Album album in AlbumsToLoad)
+                {
+                    foreach (Photo photo in album.Photos)
+                    {
+                        StringBuilder photoTags = new StringBuilder();
+                        if (photo.Tags != null)
+                        {
+                            foreach (PhotoTag tag in photo.Tags)
+                            {
+                                photoTags.Append(tag.User.Name);
+                                photoTags.Append(", ");
+                            }
+                            photoTags.Remove(photoTags.Length - 2, 2);
+                        }
+
+                        DataTable.Rows.Add(
+                            photo,
+                            photo.Album.Name,
+                            //TODO get most liked comment
+                            photo.CreatedTime != null ? photo.CreatedTime : null,   //TODO fix format
+                            photo.LikedBy != null ? photo.LikedBy.Count : 0,
+                            photo.Comments != null ? photo.Comments.Count : 0,
+                            photo.Tags != null ? photoTags.ToString() : string.Empty,
+                            photo.URL
+                            );
+                        progressBarWindow.ProgressValue++;
+                        //PhotosDataLoadStatus = (float)++counter / totalRows;
+                        //PhotosDataLoadStatus.Value++;
+                    }
                 }
             }
 
